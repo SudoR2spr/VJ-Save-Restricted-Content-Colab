91995b0c09a1c936fb091ebda05076181d3776ce
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-09-03 01:33:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,25 +3,7 @@
 # Ask Doubt on telegram @KingVJ01
 
 from main import Bot
-from flask import Flask, jsonify
 
-# Home
-@app.route('/')
-def index():
-    return 'Bot Is Alive'
-
-# Health check endpoint
-@app.route('/health', methods=['GET'])
-def health_check():
-    return jsonify(status='OK'), 200
-
-if __name__ == "__main__":
-    # Start Flask app in a separate thread
-    def run_flask():
-        app.run(host='0.0.0.0', port=8000)
-
-    flask_thread = Thread(target=run_flask)
-    flask_thread.start()
 
 Bot().run()
 
